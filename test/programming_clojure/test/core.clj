966b4eb32ae31789ada2decf968fb62f1f5563a1
Kyle Isom <coder@kyleisom.net>
2011-12-22 20:58:45 +0300

    adding clojure testing

---
+++ test/programming_clojure/test/core.clj
@@ -0,0 +1,28 @@
+(ns programming-clojure.test.core
+  (:use [programming-clojure.core])
+  (:use [programming-clojure.chapter3])
+  (:use [clojure.test]))
+
+(deftest blank-tests
+  (is (not-blank? "foo"))
+  (is (not (not-blank? "")))
+  (is (not (not-blank? "   "))))
+
+(deftest comment-tests
+  (is (not-comment? "(foo) ; comment"))
+  (is (not-comment? "(foo)"))
+  (is (not (not-comment? "; (foo) does foo"))))
+
+(deftest clojuresource-tests
+  (is (clojure-source? "core.clj"))
+  (is (clojure-source? "tests.clj"))
+  (is (not (clojure-source? "foo")))
+  (is (not (clojure-source? "foo.c")))
+  (is (not (clojure-source? (str (gensym)))))
+  (is (not (clojure-source? "foo.cljnot"))))
+
+(deftest git-tests
+  (is (git-metadata? ".git/foo"))
+  (is (git-metadata? ".gitignore"))
+  (is (not (git-metadata? "foo.clj"))))
+
