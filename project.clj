3b91cc2676fe8c12d293f8c80ea2c5f283bd2c87
Kyle Isom <coder@kyleisom.net>
2011-12-22 20:29:34 +0300

    pointing main namespace to core.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject programming-clojure "0.0.1-SNAPSHOT"
+  :main programming-clojure.core
   :description "working through Programming Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]])
--- src/programming_clojure/core.clj
+++ src/programming_clojure/core.clj
@@ -1,3 +1,13 @@
 (ns programming-clojure.core
-  (:require programming-clojure.chapter3))
+  (:use programming-clojure.chapter3))
+
+(def steveo-dir "/Users/kyle/src/steveo")
+(def this-dir "/Users/kyle/Code/programming-clojure")
+
+(println "steveo's source files: " (show-clojure-source-files steveo-dir))
+(println "source files in this project: " (show-clojure-source-files this-dir))
+
+(println "steveo's projects' SLOC:" (clojure-sloc steveo-dir))
+(println "this project's SLOC:" (clojure-sloc))
+
 
