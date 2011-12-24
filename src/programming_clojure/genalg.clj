ba25347bb5fa6e5a3ad6c95cb380c5cb43f73e8c
Kyle Isom <coder@kyleisom.net>
2011-12-24 20:20:48 +0300

    adding genetic algorithm playcode

---
+++ src/programming_clojure/genalg.clj
@@ -0,0 +1,38 @@
+;;; some genetic algorithm code I was originally messing with in python
+;;; it seems a good way to relearn thinking in lisp and good for learning clojure
+
+;;; warning: currently broken
+;;;    (trying to remember how to think functionally)
+(ns programming-clojure.ga)
+
+(defn- same-size?
+  "Returns true if both sequences are the same size."
+  [coll1 coll2]
+  (== (count coll1) (count coll2)))
+
+(defn- restr
+  [string]
+  (apply str (rest string)))
+
+(defn- valid-target?
+  "Ensures a target string is valid."
+  [target]
+  (not (and
+   (== 0 (count target))
+   (not (nil? target)))))
+   
+
+(defn- fitness
+  "Calculate fitness level of two strings. target is the desired string, source is
+   the string being evaluated."
+  [ target source & k ]
+  (println (format "target: %s\nsource: %s\n" target source))
+  (when (valid-target? target)
+    (let [x (int (first target))
+          y (int (first source))
+          k (if (empty? k) 2 (first k))]
+      (reduce +
+              (lazy-seq
+               (cons (Math/abs (Math/pow (- x y) k))
+                     (fitness (restr target) (restr source) k)))))))
+
