9d333c10b32586c6e228421e365ee3a48fc53726
Kyle Isom <coder@kyleisom.net>
2011-12-26 14:45:08 +0300

    Working on adding chapter 4 recursion examples.

--- src/programming_clojure/chapter4.clj
+++ src/programming_clojure/chapter4.clj
@@ -1,11 +1,11 @@
 (ns programming-clojure.chapter4)
 
-					; recursion with fibonacci
-					; basis: f-0 <-- 0, f-1 <-- 1
-					; induction: for n > 1, f-n = f-(n-1) + f-(n-2)
+;;; recursion with fibonacci
+;; basis: f-0 <-- 0, f-1 <-- 1
+;; induction: for n > 1, f-n = f-(n-1) + f-(n-2)
 
-(defn simple-fibo			;bad simple recursion
-  "A simple fibonacci recursion example that will blow the stack."
+(defn simple-fibo
+  "A simple fibonacci recursion example that is slow and will blow the stack."
   [n]
   (cond
    (= n 0) 0
@@ -13,19 +13,35 @@
    :else (+ (simple-fibo (- n 1))
 	    (simple-fibo (- n 2)))))
 
-(defn tail-fibo [n]
+(defn tail-fibo
+  "Fibonacci recusion example using tail recursion."
+  [n]
   (letfn [(fib [current next n]
             (if (zero? n) current
                 (fib next (+ current next) (dec n))))]
     (fib 0N 1N n)))
 
-(defn recur-fibo [n]
+(defn recur-fibo
+  "Explicit self-recursion with recur."
+  [n]
   (letfn [(fib [current next n]
             (if (zero? n) current
                 (recur next (+ current next) (dec n))))]
     (fib 0N 1N n)))
 
 (defn lazy-fibo
+  "Lazy fibonacci sequence using explicit lazy-seq."
   ([] (concat [0 1] (lazy-fibo 0N 1N)))
   ([a b] (let [n (+ a b)]
            (lazy-seq (cons n (lazy-fibo b n))))))
+
+(defn mapi-fibo
+  "Lazy sequence implicitly via map/iterate."
+  []
+  (map first                                     ; map and iterate create lazy
+       (iterate (fn [[a b]] [b (+ a b)])         ; sequences by default
+                [0N 1N])))
+
+
+
+
--- src/programming_clojure/core.clj
+++ src/programming_clojure/core.clj
@@ -1,16 +1,34 @@
 (ns programming-clojure.core
-  (:gen-class)
-  (:use programming-clojure.chapter3))
+;  (:gen-class)
+  (:use programming-clojure.chapter3)
+  (:use programming-clojure.chapter4))
 
 ; globals
 (def steveo-dir "/Users/kyle/src/steveo")
 (def this-dir "/Users/kyle/Code/programming-clojure")
 
-(defn -main [& args]
+(defn ch3_main []
   (println "steveo's source files: " (show-clojure-source-files steveo-dir))
   (println "source files in this project: " (show-clojure-source-files this-dir))
   (println "steveo's projects' SLOC:" (clojure-sloc steveo-dir))
   (println "this project's SLOC:" (clojure-sloc))
   (println "cost of steveo's project:" (format "%.2f" (project-cost steveo-dir)))
   (println "cost of this project:" (format "$%.2f" (project-cost))))
+
+
+(defn ch4_main []
+  (println "timing simple fibo:")
+  (time (simple-fibo 10))
+  (println "timing tail fibo:")
+  (time (tail-fibo 10))
+  (println "timing recur fibo:")
+  (time (recur-fibo 10))
+  (println "timing explicit lazy fibo:")
+  (time (nth (lazy-fibo) 10))
+  (println "timing implicit lazy fibo:")
+  (time (nth (mapi-fibo) 10)))
+
+(defn -main [& args]
+  (ch3_main)
+  (ch4_main))
 
